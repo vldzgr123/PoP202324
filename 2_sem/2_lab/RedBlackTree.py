7384ded8cb1abe08e0c5e852f1a359f066223131
Vladislav Zagrebelny <vlszgr@mail.ru>
2024-04-05 08:35:53 +0600

    1

--- 2_sem/2_lab/RedBlackTree.py
+++ 2_sem/2_lab/RedBlackTree.py
@@ -4,34 +4,75 @@ class Node:
         self.name = key
         self.right = None
         self.left = None
-        self.isRed = False
+        self.isRed = True
+
+    def _getGrandPa(self):
+        if self.key != None and self.parent != None:
+            return self.parent.parent
+        else:
+            return None
+
+    def _getUncle(self):
+        grandParent = self._getGrandPa()
+        if grandParent == None:
+            return None
+        if self.parent == grandParent.left:
+            return grandParent.right
+        else:
+            return grandParent.left
 
 
 class RedBlackTree:
-    def __init__(self, key):
-        rootNode = Node(key)
-        self.root = rootNode
+    def __init__(self):
+        self.root = Node(None)
+        self.root.isRed = False
 
     def Insert(self, key):
-        node = Node(key)
-        self._InsertNode(self.root, node)
-
-    def _InsertNode(self, parent, node):
-        if parent.key > node.key:
-            if parent.right is None:
-                if parent.isRed == False:
-                    node.isRed = True
-                else:
-                    node.isRed = False
-                parent.right = node
+        newNode = Node(key)
+        current = self.root
+        parent = current
+        while current.key != None:
+            parent = current
+            if newNode.key < current.key:
+                current = current.left
             else:
-                self._InsertNode(parent.right, node)
+                current = current.right
+        newNode.parent = parent
+        if parent.key < newNode.key:
+            parent.left = newNode
         else:
-            if parent.left is None:
-                if parent.isRed == False:
-                    node.isRed = True
-                else:
-                    node.isRed = False
-                parent.left = node
+            parent.right = newNode
+        self._balanceInsert(newNode)
+
+    def _getGrandPa(self, node):
+        if node != None and node.parent != None:
+            return node.parent.parent
+        else:
+            return None
+
+    def _getUncle(self, node):
+        grandParent = self._getGrandPa(node)
+        if grandParent == None:
+            return None
+        if node.parent == grandParent.left:
+            return grandParent.right
+        else:
+            return grandParent.left
+
+    def _rotateRight(self, node):
+
+        pivot = node.left
+
+        pivot.parent = node.parent
+        if node.parent != None:
+            if node.parent.left == node:
+                node.parent.left = pivot
             else:
-                self._InsertNode(parent.left, node)
+                node.parent.right = pivot
+
+        node.right = pivot.left
+        if pivot.left != None:
+            pivot.left.parent = node
+
+        node.parent = pivot
+        pivot.left = node
